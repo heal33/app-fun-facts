bace22eb9647ff24ac49b6bfe55d0adec24b0db3
duxxyz <heavenlee@me.com>
2015-10-27 12:24:29 -1000

    added arrays

--- app/src/main/java/io/maddux/funfacts/FunFactsActivity.java
+++ app/src/main/java/io/maddux/funfacts/FunFactsActivity.java
@@ -24,12 +24,28 @@ public class FunFactsActivity extends Activity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String[] facts = {
+                        "All of the Boeing 747s have traveled a collective 35 billion miles. That’s equivalent to 75,000 trips to the moon and back!",
+                        "Every day, there are approximately 200,000 flights around the world.",
+                        "Food tastes different under pressure (like in an airplane!). For example, our ability to perceive salty tastes is weakened, so tomato juice tastes sweet.",
+                        "The first time Amelia Earhart saw an airplane, she wasn’t impressed and declined a ride!",
+                        "As a plane moves, air travels over the top of the wing faster than it flows beneath the wing. This creates higher pressure beneath the wing and forces it up into the lower pressure area. This process is called “lift.”",
+                        "Orville and Wilbur Wright, America’s aviation pioneers, designed the first successful airplane.",
+                        "Most of a plane is made from lightweight aluminum. Mercury, the chemical found in thermometers, can destroy aluminum in minutes. This is why it’s forbidden to bring it on flights!",
+                        "In the 1920s, a plane ticket cost just $5!",
+                        "Air travel is the second safest form of travel. The first safest method is the elevator/escalator, although you wouldn’t get very far!",
+                        "A plane’s life is measured in the number of times it undergoes pressurization. Every time it’s pressured, stress is caused on the fuselage, which adds up to tiny cracks that can’t be repaired.",
+                };
+
+
                 //The button was clicked, so update the fact label with a new fact
                 String fact = "";
                 // Randomly select a fact
                 Random randomGenerator = new Random();  // Construct a new Random number generator
-                int randomNumber = randomGenerator.nextInt(3);
-                fact = randomNumber + "";
+                int randomNumber = randomGenerator.nextInt(facts.length);
+
+                fact = facts[randomNumber];
+
 
                 //Update the label with out dynamic fact
                 factLabel.setText(fact);
