440f92318cca2a65e9332216b7936706288bf780
duxxyz <heavenlee@me.com>
2015-10-27 13:06:55 -1000

    dynamic background color - one color

--- app/src/main/java/io/maddux/funfacts/FunFactsActivity.java
+++ app/src/main/java/io/maddux/funfacts/FunFactsActivity.java
@@ -1,11 +1,13 @@
 package io.maddux.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -23,12 +25,15 @@ public class FunFactsActivity extends Activity {
         //Declare our View Variables and assign the views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String fact = mFactBook.getFact();
                 //Update the label with out dynamic fact
                 factLabel.setText(fact);
+                relativeLayout.setBackgroundColor(Color.CYAN);
             }
         };
         showFactButton.setOnClickListener(listener);
